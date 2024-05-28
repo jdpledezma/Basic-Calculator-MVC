dcee0db9c22cdb5a81df213f8007bacf43c56a1e
Javier <jdpledezma@gmail.com>
2024-05-28 17:35:50 -0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import controller.ControllerCalculator;
 import model.ModelCalculator;
 import view.ViewCalculator;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         ModelCalculator model = new ModelCalculator();
